63657fe1d4e80e4c91a48869f7a2dcfc7a095215
Alexander Chernyakhovsky <achernya@mit.edu>
2011-12-01 00:49:07 -0500

    Better libs

--- libscan/categorize.sh
+++ libscan/categorize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#for i in $(find /lib -name "*.so");
-for i in $(find /usr/sbin -perm /+x);
+#for i in $(find /lib /lib64 /usr/lib /usr/lib64 -name "*.so*");
+for i in $(find /bin /sbin/ /usr/bin /usr/sbin -perm /+x);
 do
     PACKAGE=$(rpm -q --whatprovides $i)
     CATEGORY=$(rpm -q $PACKAGE --info | grep "^Group" | cut -d: -f2)
